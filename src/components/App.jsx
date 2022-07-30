a2d6d2f17ea4365ccd94711c3bc3df7370455e5f
OmarMo7 <omar7.first@gmail.com>
2022-07-30 07:02:40 +0200

    Creating the projects's skeleton of files & folders

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,8 +1,30 @@
 import React from 'react'
-
+import { CssBaseline } from '@mui/material'
+import { Route, Switch } from 'react-router-dom'
+import { Actors, MovieInfo, Movies, NavBar, Profile } from './'
 const App = () => {
+
   return (
-    <div>This is it</div>
+    <div>
+      <CssBaseline />
+      <NavBar />
+      <main>
+        <Switch>
+          <Route exact path={'/'}>
+            <Movies />
+          </Route>
+          <Route exact path={'/movies/:id'}>
+            <MovieInfo />
+          </Route>
+          <Route exact path={'/actors/:id'}>
+            <Actors />
+          </Route>
+          <Route exact path={'/profile/:id'}>
+            <Profile />
+          </Route>
+        </Switch>
+      </main>
+    </div>
   )
 }
 
