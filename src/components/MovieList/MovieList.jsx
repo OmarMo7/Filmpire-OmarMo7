9318d45e47038fe7b36a651e1251d75eebf55420
OmarMo7 <omar7.first@gmail.com>
2022-08-08 03:53:16 +0200

    Deleting unnecessaries

--- src/components/MovieList/MovieList.jsx
+++ src/components/MovieList/MovieList.jsx
@@ -3,35 +3,9 @@ import { CircularProgress, Grid, Typography, Box } from '@mui/material'
 
 import useStyles from './styles'
 import { Movie } from '..'
-import { useGetMoviesQuery } from '../../services/TMDB'
 
 const MovieList = ({ movies }) => {
   const classes = useStyles()
-  const { data, error, isFetching } = useGetMoviesQuery()
-
-  if (isFetching) {
-    return (
-      <Box display="flex" justifyContent="center">
-        <CircularProgress size={"4rem"} />
-      </Box>
-    )
-  }
-
-  if (!data.results.length) {
-    return (
-      <Box display="flex" alignItems="center" mt="20px">
-        <Typography variant="h4">
-          No movies with such a name
-          <br />
-          Please search for something else
-        </Typography>
-      </Box>
-    )
-  }
-
-  if (error) {
-    return 'An error has occured'
-  }
 
   return (
     <Grid container className={classes.moviesContainer}>
