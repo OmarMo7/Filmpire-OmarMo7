e0fe4a430943544f98425912e3281ab766547f02
OmarMo7 <omar7.first@gmail.com>
2022-08-09 23:40:05 +0200

    Loading movies

--- src/components/Movies/Movies.jsx
+++ src/components/Movies/Movies.jsx
@@ -5,7 +5,35 @@ import { useGetMoviesQuery } from '../../services/TMDB'
 import { MovieList } from '..'
 
 const Movies = () => {
-  const { data } = useGetMoviesQuery()
+  const [page, setPage] = useState(1)
+  const { genreIdOrCategoryName, searchQuery } = useSelector((state) => state.currentGenreOrCategory)
+  const { data, error, isFetching } = useGetMoviesQuery({ page, genreIdOrCategoryName, searchQuery })
+
+  if (isFetching) {
+    return (
+      <Box display="flex" justifyContent="center">
+        <CircularProgress size={"4rem"} />
+      </Box>
+    )
+  }
+
+  if (!data?.results.length) {
+    return (
+      <Box display="flex" alignItems="center" mt="20px">
+        <Typography variant="h4">
+          No movies with such a name
+          <br />
+          Please search for something else
+        </Typography>
+      </Box>
+    )
+  }
+
+  if (error) {
+    return 'An error has occured'
+  }
+
+
   return (
     <div>
       <MovieList movies={data} />
