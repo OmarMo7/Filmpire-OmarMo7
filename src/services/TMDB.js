fb1a4c8d6a9b793bed98f3b4365c3b050b5cf351
OmarMo7 <omar7.first@gmail.com>
2022-08-12 00:33:33 +0200

    Adding the logic of getting a single movie or genres

--- src/services/TMDB.js
+++ src/services/TMDB.js
@@ -1,7 +1,6 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
 
 const tmdbApiKey = process.env.REACT_APP_TMDB_KEY
-const page = 1
 console.log(tmdbApiKey)
 //https://api.themoviedb.org/3/movie/popular?api_key=<<api_key>>&language=en-US&page=1
 export const tmdbApi = createApi({
@@ -9,12 +8,39 @@ export const tmdbApi = createApi({
   baseQuery: fetchBaseQuery({ baseUrl: 'https://api.themoviedb.org/3' }),
   endpoints: (builder) => ({
     //Get Movies by Type
+    getGenres: builder.query({
+      query: () => `genre/movie/list?&api_key=${tmdbApiKey}`
+    }),
+    //Get Movies by Type
     getMovies: builder.query({
-      query: () => `movie/popular?page=${page}&api_key=${tmdbApiKey}`
-    })
+      query: ({ page, genreIdOrCategoryName, searchQuery }) => {
+        //* Get Movies By Search
+        if (searchQuery) {
+          return `/search/movie?query=${searchQuery}&page=${page}&api_key=${tmdbApiKey}`
+        }
+
+        //* Get Movies By Category
+        if (genreIdOrCategoryName && typeof genreIdOrCategoryName === 'string') {
+          return `movie/${genreIdOrCategoryName}?page=${page}&api_key=${tmdbApiKey}`
+        }
+
+        //* Get Movies By Genre
+        if (genreIdOrCategoryName && typeof genreIdOrCategoryName === 'number') {
+          return `discover/movie?with_genres=${genreIdOrCategoryName}&page=${page}&api_key=${tmdbApiKey}`
+        }
+
+        //* Get Popular Movies By default
+        return `movie/popular?page=${page}&api_key=${tmdbApiKey}`
+      }
+    }),
+    getMovie: builder.query({
+      query: (id) => `movie/${id}?append_to_response=videos,credits&api_key=${tmdbApiKey}`
+    }),
   })
 })
 
 export const {
+  useGetGenresQuery,
   useGetMoviesQuery,
+  useGetMovieQuery,
 } = tmdbApi
