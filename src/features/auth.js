204c9d2b95fc732dbd2f92eb5f43b10f918fb450
OmarMo7 <omar7.first@gmail.com>
2022-08-12 22:36:09 +0200

    Configuring the auth slice

---
+++ src/features/auth.js
@@ -0,0 +1,23 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+  user: {},
+  isAuth: false,
+  session_id: ""
+}
+
+const authSlice = createSlice({
+  name: 'user',
+  initialState,
+  reducers: {
+    setUser: (state, action) => {
+        state.user = action.payload,
+        state.isAuth = true,
+        state.session_id = localStorage.getItem('session_id')
+    }
+  }
+})
+
+export const { setUser } = authSlice.actions
+
+export default authSlice.reducer
