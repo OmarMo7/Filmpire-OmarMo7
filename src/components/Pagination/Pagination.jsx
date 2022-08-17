dd96a04ec4e8ba680e8140983d01983f76deedff
OmarMo7 <omar7.first@gmail.com>
2022-08-17 22:57:48 +0200

    Adding pagination comp

---
+++ src/components/Pagination/Pagination.jsx
@@ -0,0 +1,38 @@
+import { Typography, Button } from '@mui/material'
+import React, { useRef } from 'react'
+import useStyles from './styles'
+
+const Pagination = ({ currentPage, setPage, totalPages }) => {
+
+  if (totalPages === 0) return null;
+
+  const classes = useStyles()
+
+  const handlePrev = () => {
+    if (currentPage !== 1) {
+      setPage((prevPage) => prevPage - 1)
+    }
+  }
+
+  const handleNext = () => {
+    if (currentPage !== totalPages) {
+      setPage((prevPage) => prevPage + 1)
+    }
+  }
+
+  return (
+    <div className={classes.container}>
+      <Button color="primary" onClick={handlePrev} type="button" className={classes.button} variant="contained">
+        Prev
+      </Button>
+      <Typography variant='h4' className={classes.pageNumber}>
+        {currentPage}
+      </Typography>
+      <Button color="primary" onClick={handleNext} type="button" className={classes.button} variant="contained">
+        Next
+      </Button>
+    </div>
+  )
+}
+
+export default Pagination
