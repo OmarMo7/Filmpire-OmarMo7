90b4bf56574dd05d9609fcc218e07ccbaa2e7804
OmarMo7 <omar7.first@gmail.com>
2022-08-24 00:40:43 +0200

    Coniguring the sidebar moving

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,24 +1,45 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { CssBaseline } from '@mui/material'
 import { Route, Switch } from 'react-router-dom'
 import { Actors, MovieInfo, Movies, NavBar, Profile } from './'
+import { useTheme } from '@mui/material/styles'
+
 import useStyles from './styles'
+
 
 const App = () => {
   const classes = useStyles()
+  const [mobileOpen, setMobileOpen] = useState(false)
+  const theme = useTheme()
 
+  const propsOnClose = {
+    flexGrow: 1,
+    transition: theme.transitions.create('margin', {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.leavingScreen,
+    }),
+    marginLeft: `0px`,
+  }
+
+  const propsOnOpen = {
+    transition: theme.transitions.create('margin', {
+      easing: theme.transitions.easing.easeOut,
+      duration: theme.transitions.duration.enteringScreen,
+    }),
+    marginLeft: `240px`,
+  }
 
   return (
     <div className={classes.root}>
       <CssBaseline />
-      <NavBar />
-      <main className={classes.content}>
+      <NavBar mobileOpen={mobileOpen} setMobileOpen={setMobileOpen} />
+      <main className={classes.content} style={mobileOpen ? propsOnOpen : propsOnClose}>
         <div className={classes.toolbar} />
         <Switch>
           <Route exact path={'/'}>
             <Movies />
           </Route>
-          <Route exact path={'/movies/:id'}>
+          <Route exact path={'/movie/:id'}>
             <MovieInfo />
           </Route>
           <Route exact path={'/actors/:id'}>
--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -5,12 +5,11 @@ import { useTheme } from '@mui/material/styles'
 import useStyles from './styles'
 import { useGetGenresQuery } from '../../services/TMDB'
 import genreIcons from '../../assets/assets/genres'
-import { useDispatch, useSelector } from 'react-redux'
+import { useDispatch, } from 'react-redux'
 import { selectGenreOrCategory } from '../../features/currentGenreOrCategory'
-const Sidebar = ({ setMobileOpen }) => {
+const Sidebar = () => {
 
   const { data, isFetching } = useGetGenresQuery()
-  console.log(data)
   const redLogo = 'https://fontmeme.com/permalink/210930/8531c658a743debe1e1aa1a2fc82006e.png';
   const blueLogo = 'https://fontmeme.com/permalink/210930/6854ae5c7f76597cf8680e48a2c8a50a.png';
   const theme = useTheme()
@@ -23,12 +22,14 @@ const Sidebar = ({ setMobileOpen }) => {
 
   ]
 
+  console.log(theme.palette.mode)
+
   return (
     <>
       <Link to='/' className={classes.imageLink}>
         <img
           className={classes.image}
-          src={theme.palette.mode === 'light' ? blueLogo : redLogo}
+          src={theme.palette.mode === 'light' ? redLogo : blueLogo}
           alt="Filmpire"
         />
       </Link>
