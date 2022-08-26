80050973f24a5626e6152aeabfda75b186817387
OmarMo7 <omar7.first@gmail.com>
2022-08-26 19:56:43 +0200

    Adding the RatedCard comp for the WL and fav

---
+++ src/components/RatedCards/RatedCards.jsx
@@ -0,0 +1,35 @@
+import React from 'react'
+import { Typography, Box } from '@mui/material'
+import useStyles from './styles'
+import { Movie } from '..'
+
+const RatedCards = ({ title, data }) => {
+  const classes = useStyles()
+  console.log(data?.results)
+
+  return (
+    <div>
+      <Box>
+        <Typography variant='h5' gutterBottom >
+          {title}
+        </Typography>
+        {data?.results?.length !== 0 ? (
+          <Box display="flex" flexWrap={"wrap"} className={classes.container}>
+            {data?.results.map((movie, i) => (
+              <Movie key={movie.id} movie={movie} i={i} />
+            ))}
+          </Box>
+
+        ) :
+          (
+            <div style={{ marginLeft: '10px' }}>
+              No movies to show
+            </div>
+          )
+        }
+      </Box>
+    </div>
+  )
+}
+
+export default RatedCards
